943ab9f713585b071bb70cd469a15c57bd7987c6
wizardjaja <599269930@qq.com>
2020-02-17 17:14:59 +0800

    在maven工程中自定义 mybatis-注解 方式的框架

--- src/main/java/com/wizard/mapper/IUserMapper.java
+++ src/main/java/com/wizard/mapper/IUserMapper.java
@@ -1,9 +1,13 @@
 package com.wizard.mapper;
 
 import com.wizard.model.User;
+import com.wizard.mybatis.annotations.Select;
 
 import java.util.List;
 
 public interface IUserMapper {
+//    List<User> getAllUsers();
+
+    @Select("select * from tab_user")
     List<User> getAllUsers();
 }
